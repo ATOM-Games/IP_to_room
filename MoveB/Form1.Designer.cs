3f0d7721faf8d133f3f17847a2875ca48d7c05e8
ATOM-Games <silatrotila0atom@gmail.com>
2021-03-26 10:59:55 +0300

    IP to room version 1.0

---
+++ MoveB/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace MoveB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TEXTA = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TEXTA
+            // 
+            this.TEXTA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TEXTA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TEXTA.Location = new System.Drawing.Point(0, 0);
+            this.TEXTA.Name = "TEXTA";
+            this.TEXTA.ReadOnly = true;
+            this.TEXTA.Size = new System.Drawing.Size(284, 30);
+            this.TEXTA.TabIndex = 0;
+            this.TEXTA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 31);
+            this.Controls.Add(this.TEXTA);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "КОМНАТА";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TEXTA;
+
+    }
+}
+
---
+++ MoveB/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace MoveB
+{
+    public partial class Form1 : Form
+    {
+        string Host, IP, pubIp;
+        string str = "";
+        public Form1()
+        {
+            InitializeComponent();
+            GetIP();
+        }
+        void GetIP() {
+            try
+            {
+                pubIp = new System.Net.WebClient().DownloadString("https://api.ipify.org");
+                TEXTA.Text = IP_HS(pubIp);
+            }
+            catch (Exception yy) {
+                TEXTA.Text = "ERROR";
+            }
+        }
+        public string IP_HS(string IP)
+        {
+            string str = "";
+            string[] ipi = IP.Split('.');
+            int[] ini = new int[ipi.Length];
+            for (int i = 0; i < ini.Length; ini[i] = System.Convert.ToInt32(ipi[i]), i++) ;
+            for (int i = 0; i < ini.Length; str += ini[i].ToString("X"), i++) ;
+            str = str.Replace('0', 'Z').Replace('1', 'O').Replace('2', 'T').Replace('3', 'W').Replace('4', 'X').Replace('5', 'P').Replace('6', 'S').Replace('7', 'V').Replace('8', 'H').Replace('9', 'N');
+            return str;
+        }
+
+
+        public string HS_IP(string SH)
+        {
+            string str = SH;
+            str = str.Replace('Z', '0').Replace('O', '1').Replace('T', '2').Replace('W', '3').Replace('X', '4').Replace('P', '5').Replace('S', '6').Replace('V', '7').Replace('H', '8').Replace('N', '9');
+            char[] ipi = str.ToCharArray();
+            int[] ini = new int[4];
+            ini[0] = System.Convert.ToInt32(str.Substring(0, 2), 16);
+            ini[1] = System.Convert.ToInt32(str.Substring(2, 2), 16);
+            ini[2] = System.Convert.ToInt32(str.Substring(4, 2), 16);
+            ini[3] = System.Convert.ToInt32(str.Substring(6, 2), 16);
+            return ini[0] + "." + ini[1] + "." + ini[2] + "." + ini[3];
+        }
+    }
+}
